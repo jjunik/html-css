2a45cbc76d0fe101c7cc886fbae7f55789a22039
jjunik <tjwnsdlr4545@gmail.com>
2024-09-11 17:43:19 +0900

    react

---
+++ react-workspace/todo-react-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App'; // APP 컴포넌트를 import
+import reportWebVitals from './reportWebVitals';
+
+// React18 부터 React.render() 가 아닌 createRoot()를 먼저 사용
+// <컴포넌트 명/>을 이용해 컴포넌트를 사용한다.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(// ReactDOM이 내부 컴포넌트들을 'root' 엘리먼트에 render 함수
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
