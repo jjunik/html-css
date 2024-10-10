a426cf98ab4b575ad7d5095e56fa58a4d7e7ab93
jjunik <tjwnsdlr4545@gmail.com>
2024-10-10 17:49:39 +0900

    react product

---
+++ react-workspace/product-react-app/src/AddProduct.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function AddProduct(props){
+    const [product, setProduct] = useState({name : "", stock : 0, price : 0});
+
+    const{name,stock,price} = product;
+
+    
+
+}
